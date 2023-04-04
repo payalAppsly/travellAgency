9c9990778a364972d8372fb943ec0be86c7015fd
payalAppsly <124361062+payalAppsly@users.noreply.github.com>
2023-04-04 12:02:21 +0530

    Category Area Design

--- src/component/Category/Category.jsx
+++ src/component/Category/Category.jsx
@@ -4,7 +4,6 @@ import Cat2 from "../../assets/cat2.png";
 import Cat3 from "../../assets/cat3.png";
 import Cat4 from "../../assets/cat4.png";
 import CatBack from "../../assets/catBack.png";
-import Cat2Back from "../../assets/cat2Back.png";
 
 const Category = () => {
   return (
@@ -31,102 +30,99 @@ const Category = () => {
             We Offer Best Services
           </h2>
         </div>
-        <img
-          src={Cat2Back}
-          alt=""
-          style={{
-            position: "relative",
-            zIndex: "-1",
-            top: "340px",
-            left: "430px",
-          }}
-        />
-        <div className="flex w-10/12 mx-auto justify-evenly items-baseline mb-32">
-          <div className="w-1/5 text-center p-2">
-            <img
-              src={Cat1}
-              alt=""
-              className="max-w-full mx-auto"
-              width={80}
-              height={80}
-            />
-            <h5
-              className="text-xl font-semibold"
-              style={{ color: "#1E1D4C", fontFamily: "Open Sans" }}
-            >
-              Calculated Weather{" "}
-            </h5>
-            <p
-              className="text-base font-medium"
-              style={{ color: "#5E6282", fontFamily: "Poppins" }}
-            >
-              Built Wicket longer admire do barton vanity itself do in it.
-            </p>
+        <div className="catArea flex w-10/12 mx-auto justify-evenly items-baseline mb-32">
+          <div className="catArea2 w-1/5 p-5">
+            <div className="text-center p-2">
+              <img
+                src={Cat1}
+                alt=""
+                className="max-w-full mx-auto"
+                width={80}
+                height={80}
+              />
+              <h5
+                className="text-xl font-semibold"
+                style={{ color: "#1E1D4C", fontFamily: "Open Sans" }}
+              >
+                Calculated Weather{" "}
+              </h5>
+              <p
+                className="text-base font-medium"
+                style={{ color: "#5E6282", fontFamily: "Poppins" }}
+              >
+                Built Wicket longer admire do barton vanity itself do in it.
+              </p>
+            </div>
           </div>
-          <div className="w-1/5 text-center p-8 rounded-xl bg-white shadow-2xl ">
-            <img
-              src={Cat2}
-              alt=""
-              className="max-w-full mx-auto"
-              width={150}
-              height={150}
-            />
-            <h5
-              className="text-xl font-semibold"
-              style={{ color: "#1E1D4C", fontFamily: "Open Sans" }}
-            >
-              Best Flights
-            </h5>
-            <p
-              className="text-base font-medium"
-              style={{ color: "#5E6282", fontFamily: "Poppins" }}
-            >
-              Engrossed listening. Park gate sell they west hard for the.
-            </p>
+          <div className="catArea2 w-1/5 p-5">
+            <div className="text-center p-2">
+              <img
+                src={Cat2}
+                alt=""
+                className="max-w-full mx-auto"
+                width={150}
+                height={150}
+              />
+              <h5
+                className="text-xl font-semibold"
+                style={{ color: "#1E1D4C", fontFamily: "Open Sans" }}
+              >
+                Best Flights
+              </h5>
+              <p
+                className="text-base font-medium"
+                style={{ color: "#5E6282", fontFamily: "Poppins" }}
+              >
+                Engrossed listening. Park gate sell they west hard for the.
+              </p>
+            </div>
           </div>
-
-          <div className="w-1/5 text-center p-2">
-            <img
-              src={Cat3}
-              alt=""
-              className="max-w-full mx-auto"
-              width={70}
-              height={75}
-            />
-            <h5
-              className="text-xl font-semibold"
-              style={{ color: "#1E1D4C", fontFamily: "Open Sans" }}
-            >
-              Local Events
-            </h5>
-            <p
-              className="text-base font-medium"
-              style={{ color: "#5E6282", fontFamily: "Poppins" }}
-            >
-              Barton vanity itself do in it. Preferd to men it engrossed
-              listening.{" "}
-            </p>
+          <div className="catArea2 w-1/5 p-5">
+            <div className="text-center p-2">
+              <img
+                src={Cat3}
+                alt=""
+                className="max-w-full mx-auto"
+                width={70}
+                height={75}
+              />
+              <h5
+                className="text-xl font-semibold"
+                style={{ color: "#1E1D4C", fontFamily: "Open Sans" }}
+              >
+                Local Events
+              </h5>
+              <p
+                className="text-base font-medium"
+                style={{ color: "#5E6282", fontFamily: "Poppins" }}
+              >
+                Barton vanity itself do in it. Preferd to men it engrossed
+                listening.{" "}
+              </p>
+            </div>
           </div>
-          <div className="w-1/5 text-center p-2">
-            <img
-              src={Cat4}
-              alt=""
-              className="max-w-full mx-auto"
-              width={80}
-              height={80}
-            />
-            <h5
-              className="text-xl font-semibold"
-              style={{ color: "#1E1D4C", fontFamily: "Open Sans" }}
-            >
-              Customization
-            </h5>
-            <p
-              className="text-base font-medium"
-              style={{ color: "#5E6282", fontFamily: "Poppins" }}
-            >
-              We deliver outsourced aviation services for military customers
-            </p>
+          <div className="catArea2 w-1/5 p-5">
+            <div className="text-center p-2">
+              <img
+                src={Cat4}
+                alt=""
+                className="max-w-full mx-auto"
+                width={80}
+                height={80}
+              />
+              <h5
+                className="text-xl font-semibold"
+                style={{ color: "#1E1D4C", fontFamily: "Open Sans" }}
+              >
+                Customization
+              </h5>
+              <p
+                className="text-base font-medium"
+                style={{ color: "#5E6282", fontFamily: "Poppins" }}
+              >
+                We deliver outsourced aviation services for military customers
+              </p>
+            </div>
           </div>
         </div>
       </div>
